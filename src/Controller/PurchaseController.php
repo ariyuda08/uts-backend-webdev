6788f28a003a41aa7a554f6ca9848a0524bafc06
ariyuda08 <220040126@stikom-bali.ac.id>
2024-05-01 09:13:34 +0800

    fixed a minor error in the PurchaseController.php file

--- src/Controller/PurchaseController.php
+++ src/Controller/PurchaseController.php
@@ -1,4 +1,4 @@
-]<?php
+<?php
 require_once __DIR__ . '/../Model/Purchases.php';
 
 class PurchaseController {
@@ -10,27 +10,29 @@ class PurchaseController {
 
     public function getAllPurchases() {
         $purchases = $this->purchaseModel->getAllPurchases();
-        return $purchases;
+        header('Content-Type: application/json');
+        echo json_encode($purchases);
     }
 
-    public function getPurchaseById($purchaseId) {
-        $purchase = $this->purchaseModel->getPurchaseById($purchaseId);
-        return $purchase;
+    public function getPurchaseById($id) {
+        $purchase = $this->purchaseModel->getPurchaseById($id);
+        header('Content-Type: application/json');
+        echo json_encode($purchase);
     }
 
     public function addPurchase($data) {
         $this->purchaseModel->addPurchase($data);
-        return "Pembelian berhasil ditambahkan";
+        echo "Pembelian berhasil ditambahkan";
     }
 
-    public function updatePurchase($purchaseId, $data) {
-        $this->purchaseModel->updatePurchase($purchaseId, $data);
-        return "Pembelian berhasil diperbarui";
+    public function updatePurchase($id, $data) {
+        $this->purchaseModel->updatePurchase($id, $data);
+        echo "Pembelian berhasil diperbarui";
     }
 
-    public function deletePurchase($purchaseId) {
-        $this->purchaseModel->deletePurchase($purchaseId);
-        return "Pembelian berhasil dihapus";
+    public function deletePurchase($id) {
+        $this->purchaseModel->deletePurchase($id);
+        echo "Pembelian berhasil dihapus";
     }
 }
 ?>
