e9ccf2bd86a104e499ada778ba6b759087fc1373
ariyuda08 <220040126@stikom-bali.ac.id>
2024-05-03 12:29:33 +0800

    update the index.php file in the public folder

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UTS Back-End Web Developer</title>
+    <title>UTS Back-End Web Development</title>
 </head>
 <body>
     <h1>XYZ Sales API End Point</h1>
